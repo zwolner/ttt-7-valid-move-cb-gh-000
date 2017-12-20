32bd3e28a573b2462e98ce0996f5dfa1325aeb4f
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-20 16:39:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   valid = nil
-  if taken
+  if index.between?(0, 8) && position_taken?(board,index)
     valid = true
   else
     valid = false
